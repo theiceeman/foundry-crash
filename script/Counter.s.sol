5ce6fdaca60c3a7172bdbc8b3e71eea43d133df3
okorieebube <okorieebube1@gmail.com>
2024-04-09 16:26:03 +0100

    setup: initial commit

--- script/Counter.s.sol
+++ script/Counter.s.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {Script, console} from "forge-std/Script.sol";
+import {Script, console} from "lib/forge-std/src/Script.sol";
+import {Counter} from "../src/Counter.sol";
 
 contract CounterScript is Script {
     function setUp() public {}
 
-    function run() public {
-        vm.broadcast();
+    function run() external returns (Counter) {
+        vm.startBroadcast();
+        Counter counter = new Counter();
+        vm.stopBroadcast();
+        return counter;
     }
 }
